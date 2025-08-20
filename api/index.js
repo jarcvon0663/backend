5dc0d9480a565c8571f78bc875302a3e5c14cda2
Jeison Arturo Rios Castaño <55066370+jarcvon0663@users.noreply.github.com>
2025-08-19 22:25:39 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,1 @@
-// api/index.js
-const suma = require("../suma");
-const obtenerTiempo = require("../tiempo");
 
-module.exports = (req, res) => {
-  if (req.url === "/suma") {
-    const resultado = suma(10, 5);
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`La suma es: ${resultado}`);
-  } else if (req.url === "/tiempo") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`Tiempo actual: ${obtenerTiempo()}`);
-  } else {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Servidor funcionando en Vercel con CommonJS 🚀");
-  }
-};
